dd633ac2f2b350bb3f59902c93d8393dc281c0d9
Automoroboto <roberto@studiodespirito.co.uk>
2023-06-08 14:48:43 +0100

    updating records

--- src/Components/SmoothieCard.js
+++ src/Components/SmoothieCard.js
@@ -1,4 +1,4 @@
-
+import { Link } from 'react-router-dom';
 
 const SmoothieCard = ({ smoothie }) => {    
     return (
@@ -6,8 +6,11 @@ const SmoothieCard = ({ smoothie }) => {
             <h3>{smoothie.title}</h3>
             <p>{smoothie.method}</p>
             <div className="rating">{smoothie.rating}</div>
+            
             <div className="buttons">
-                
+                <Link to={'/' + smoothie.id}> 
+                <i className="material-icons">edit</i>
+                </Link>
             </div>
         </div>
     )
